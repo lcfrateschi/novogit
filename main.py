754327f693b9146f465047ddd104accf6464946d
lcfrateschi@hotmail.com <littlenose123>
2022-07-17 19:24:52 -0300

    gggggggg

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def print_hi(name):
 #askdaskdasd
 #eeeeeeeeeeeeeeeeee
 #fffffffffffff
+#gggggggggggg
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
